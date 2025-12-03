c4909f2d9df4a86cf63188da464f655c33ee3222
Kellymon-xd <kellysteffany10@gmail.com>
2025-12-03 16:09:20 -0500

    Se agrega eliminacion de usuarios y traida de datos para el formulario de edicion

--- ConsumirLogin/FrmGetUsuarios.Designer.cs
+++ ConsumirLogin/FrmGetUsuarios.Designer.cs
@@ -30,17 +30,19 @@
         {
             this.lblUsuarios = new System.Windows.Forms.Label();
             this.dgvUsuarios = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.ckbActivo = new System.Windows.Forms.CheckBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtCedula = new System.Windows.Forms.TextBox();
+            this.lblCedula = new System.Windows.Forms.Label();
+            this.ckbBloqueado = new System.Windows.Forms.CheckBox();
             this.btnEditar = new System.Windows.Forms.Button();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.lblTelefono = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).BeginInit();
             this.SuspendLayout();
             // 
@@ -62,116 +64,137 @@
             this.dgvUsuarios.Name = "dgvUsuarios";
             this.dgvUsuarios.Size = new System.Drawing.Size(537, 271);
             this.dgvUsuarios.TabIndex = 13;
+            this.dgvUsuarios.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUsuarios_CellClick);
+            this.dgvUsuarios.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUsuarios_CellContentClick);
             // 
-            // label1
+            // lblNombre
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 90);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 13);
-            this.label1.TabIndex = 14;
-            this.label1.Text = "NOMBRE:";
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(28, 90);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(57, 13);
+            this.lblNombre.TabIndex = 14;
+            this.lblNombre.Text = "NOMBRE:";
             // 
-            // textBox1
+            // txtNombre
             // 
-            this.textBox1.Location = new System.Drawing.Point(91, 87);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(164, 20);
-            this.textBox1.TabIndex = 15;
+            this.txtNombre.Location = new System.Drawing.Point(91, 87);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(164, 20);
+            this.txtNombre.TabIndex = 15;
             // 
-            // checkBox1
+            // ckbActivo
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(58, 209);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(65, 17);
-            this.checkBox1.TabIndex = 16;
-            this.checkBox1.Text = "ACTIVO";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.ckbActivo.AutoSize = true;
+            this.ckbActivo.Location = new System.Drawing.Point(56, 257);
+            this.ckbActivo.Name = "ckbActivo";
+            this.ckbActivo.Size = new System.Drawing.Size(65, 17);
+            this.ckbActivo.TabIndex = 16;
+            this.ckbActivo.Text = "ACTIVO";
+            this.ckbActivo.UseVisualStyleBackColor = true;
             // 
-            // textBox2
+            // txtApellido
             // 
-            this.textBox2.Location = new System.Drawing.Point(91, 113);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(164, 20);
-            this.textBox2.TabIndex = 18;
+            this.txtApellido.Location = new System.Drawing.Point(91, 113);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(164, 20);
+            this.txtApellido.TabIndex = 18;
             // 
-            // label2
+            // lblApellido
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(23, 116);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 13);
-            this.label2.TabIndex = 17;
-            this.label2.Text = "APELLIDO:";
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(23, 116);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(62, 13);
+            this.lblApellido.TabIndex = 17;
+            this.lblApellido.Text = "APELLIDO:";
             // 
-            // textBox3
+            // txtEmail
             // 
-            this.textBox3.Location = new System.Drawing.Point(91, 139);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(164, 20);
-            this.textBox3.TabIndex = 20;
+            this.txtEmail.Location = new System.Drawing.Point(91, 139);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(164, 20);
+            this.txtEmail.TabIndex = 20;
             // 
-            // label3
+            // lblEmail
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(43, 142);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(42, 13);
-            this.label3.TabIndex = 19;
-            this.label3.Text = "EMAIL:";
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(43, 142);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(42, 13);
+            this.lblEmail.TabIndex = 19;
+            this.lblEmail.Text = "EMAIL:";
             // 
-            // textBox4
+            // txtCedula
             // 
-            this.textBox4.Location = new System.Drawing.Point(91, 165);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(164, 20);
-            this.textBox4.TabIndex = 22;
+            this.txtCedula.Location = new System.Drawing.Point(91, 165);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.Size = new System.Drawing.Size(164, 20);
+            this.txtCedula.TabIndex = 22;
             // 
-            // label4
+            // lblCedula
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(32, 168);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 13);
-            this.label4.TabIndex = 21;
-            this.label4.Text = "CÉDULA:";
+            this.lblCedula.AutoSize = true;
+            this.lblCedula.Location = new System.Drawing.Point(32, 168);
+            this.lblCedula.Name = "lblCedula";
+            this.lblCedula.Size = new System.Drawing.Size(53, 13);
+            this.lblCedula.TabIndex = 21;
+            this.lblCedula.Text = "CÉDULA:";
             // 
-            // checkBox2
+            // ckbBloqueado
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(129, 209);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(93, 17);
-            this.checkBox2.TabIndex = 23;
-            this.checkBox2.Text = "BLOQUEADO";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.ckbBloqueado.AutoSize = true;
+            this.ckbBloqueado.Location = new System.Drawing.Point(127, 257);
+            this.ckbBloqueado.Name = "ckbBloqueado";
+            this.ckbBloqueado.Size = new System.Drawing.Size(93, 17);
+            this.ckbBloqueado.TabIndex = 23;
+            this.ckbBloqueado.Text = "BLOQUEADO";
+            this.ckbBloqueado.UseVisualStyleBackColor = true;
             // 
             // btnEditar
             // 
-            this.btnEditar.Location = new System.Drawing.Point(91, 245);
+            this.btnEditar.Location = new System.Drawing.Point(89, 293);
             this.btnEditar.Name = "btnEditar";
             this.btnEditar.Size = new System.Drawing.Size(75, 23);
             this.btnEditar.TabIndex = 24;
             this.btnEditar.Text = "EDITAR";
             this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(91, 201);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(164, 20);
+            this.txtTelefono.TabIndex = 26;
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Location = new System.Drawing.Point(23, 204);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(67, 13);
+            this.lblTelefono.TabIndex = 25;
+            this.lblTelefono.Text = "TELEFONO:";
             // 
             // FrmGetUsuarios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(853, 360);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.lblTelefono);
             this.Controls.Add(this.btnEditar);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ckbBloqueado);
+            this.Controls.Add(this.txtCedula);
+            this.Controls.Add(this.lblCedula);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.ckbActivo);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
             this.Controls.Add(this.dgvUsuarios);
             this.Controls.Add(this.lblUsuarios);
             this.Name = "FrmGetUsuarios";
@@ -187,16 +210,18 @@
 
         private System.Windows.Forms.Label lblUsuarios;
         private System.Windows.Forms.DataGridView dgvUsuarios;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.CheckBox ckbActivo;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtCedula;
+        private System.Windows.Forms.Label lblCedula;
+        private System.Windows.Forms.CheckBox ckbBloqueado;
         private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.Label lblTelefono;
     }
 }
--- ConsumirLogin/FrmGetUsuarios.cs
+++ ConsumirLogin/FrmGetUsuarios.cs
@@ -2,10 +2,6 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -17,6 +13,8 @@ namespace ConsumirLogin
     public partial class FrmGetUsuarios : Form
     {
         private readonly HttpClient cliente = new HttpClient();
+        private string usuarioSeleccionadoId = null;
+
         public FrmGetUsuarios()
         {
             InitializeComponent();
@@ -24,15 +22,24 @@ namespace ConsumirLogin
             cliente.BaseAddress = new Uri("https://localhost:7193/api/");
             cliente.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/json"));
+
+            dgvUsuarios.ReadOnly = true;
+            dgvUsuarios.AllowUserToAddRows = false;
+            dgvUsuarios.AllowUserToDeleteRows = false;
         }
 
         private async void FrmGetUsuarios_Load(object sender, EventArgs e)
+        {
+            await CargarUsuarios();
+        }
+
+        private async Task CargarUsuarios()
         {
             var response = await cliente.GetAsync("Usuarios");
 
             if (!response.IsSuccessStatusCode)
             {
-                MessageBox.Show("Error fetching users");
+                MessageBox.Show("Error al obtener los usuarios");
                 return;
             }
 
@@ -40,6 +47,156 @@ namespace ConsumirLogin
             var usuarios = JsonConvert.DeserializeObject<List<MostrarUsuarioDTO>>(json);
 
             dgvUsuarios.DataSource = usuarios;
+
+            if (!dgvUsuarios.Columns.Contains("btnEliminar"))
+            {
+                DataGridViewButtonColumn btnEliminar = new DataGridViewButtonColumn();
+                btnEliminar.HeaderText = "Eliminar";
+                btnEliminar.Name = "btnEliminar";
+                btnEliminar.Text = "X";
+                btnEliminar.Width = 50;
+                btnEliminar.UseColumnTextForButtonValue = true;
+
+                dgvUsuarios.Columns.Add(btnEliminar);
+            }
+        }
+
+        // ------------------------
+        // CARGAR DATOS AL CLICK
+        // ------------------------
+        private async void dgvUsuarios_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+
+            var fila = dgvUsuarios.Rows[e.RowIndex].DataBoundItem as MostrarUsuarioDTO;
+
+            usuarioSeleccionadoId = fila.Id_Usuario;
+
+            txtNombre.Text = fila.Nombre;
+            txtApellido.Text = fila.Apellido;
+            txtEmail.Text = fila.Email;
+            txtCedula.Text = fila.Cedula;
+
+            ckbActivo.Checked = fila.Activo;
+            ckbBloqueado.Checked = fila.Bloqueado;
+
+            var response = await cliente.GetAsync($"Usuarios/{usuarioSeleccionadoId}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("No se pudo obtener detalle del usuario.");
+                return;
+            }
+
+            string json = await response.Content.ReadAsStringAsync();
+
+            var detalle = JsonConvert.DeserializeObject<dynamic>(json);
+
+            txtTelefono.Text = detalle.telefono;
+        }
+
+
+        // ------------------------
+        // BOTÓN EDITAR
+        // ------------------------
+        private async void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (usuarioSeleccionadoId == null)
+            {
+                MessageBox.Show("Seleccione un usuario primero.");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtNombre.Text) ||
+                string.IsNullOrWhiteSpace(txtApellido.Text) ||
+                string.IsNullOrWhiteSpace(txtEmail.Text) ||
+                string.IsNullOrWhiteSpace(txtTelefono.Text))
+            {
+                MessageBox.Show("Todos los campos deben estar completos.");
+                return;
+            }
+
+            // ------------------------
+            // PUT (actualizar datos)
+            // ------------------------
+            var bodyPut = new
+            {
+                nombre = txtNombre.Text,
+                apellido = txtApellido.Text,
+                email = txtEmail.Text,
+                telefono = txtTelefono.Text
+            };
+
+            string jsonPut = JsonConvert.SerializeObject(bodyPut);
+            var contenidoPut = new StringContent(jsonPut, Encoding.UTF8, "application/json");
+
+            var responsePut = await cliente.PutAsync($"Usuarios/{usuarioSeleccionadoId}", contenidoPut);
+
+
+            // ------------------------
+            // PATCH ACTIVO
+            // ------------------------
+            var bodyActivo = JsonConvert.SerializeObject(ckbActivo.Checked);
+            var contenidoActivo = new StringContent(bodyActivo, Encoding.UTF8, "application/json");
+
+            var responseActivo = await cliente.PatchAsync(
+                $"Usuarios/{usuarioSeleccionadoId}/activo",
+                contenidoActivo
+            );
+
+            // ------------------------
+            // PATCH BLOQUEADO
+            // ------------------------
+            var bodyBloq = JsonConvert.SerializeObject(ckbBloqueado.Checked);
+            var contenidoBloq = new StringContent(bodyBloq, Encoding.UTF8, "application/json");
+
+            var responseBloq = await cliente.PatchAsync(
+                $"Usuarios/{usuarioSeleccionadoId}/bloqueado",
+                contenidoBloq
+            );
+
+
+            if (responsePut.IsSuccessStatusCode &&
+                responseActivo.IsSuccessStatusCode &&
+                responseBloq.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Usuario actualizado correctamente.");
+                await CargarUsuarios();
+            }
+            else
+            {
+                MessageBox.Show("Error al actualizar usuario.");
+            }
+        }
+
+        // ------------------------
+        // ELIMINAR
+        // ------------------------
+        private async void dgvUsuarios_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvUsuarios.Columns[e.ColumnIndex].Name != "btnEliminar") return;
+
+            var fila = dgvUsuarios.Rows[e.RowIndex].DataBoundItem as MostrarUsuarioDTO;
+
+            DialogResult confirmar = MessageBox.Show(
+                "¿Eliminar este usuario?",
+                "Confirmación",
+                MessageBoxButtons.YesNo
+            );
+
+            if (confirmar == DialogResult.No) return;
+
+            var response = await cliente.DeleteAsync($"Usuarios/{fila.Id_Usuario}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Usuario eliminado.");
+                await CargarUsuarios();
+            }
+            else
+            {
+                MessageBox.Show("No se pudo eliminar.");
+            }
         }
     }
 }
